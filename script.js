1fa66cc9a7fb161c1fe5548b4c3fd1086578679e
Fernando <jfernandomora@outlook.com>
2021-08-08 08:27:23 +0800

    Add button and changed title color

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function draw() {
           ctx.beginPath();
           var x = 25 + j * 50; // x coordinate
           var y = 25 + i * 50; // y coordinate
-          var radius = 20; // Arc radius
+          var radius = 30; // Arc radius
           var startAngle = 0; // Starting point on circle
           var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
           var counterclockwise = i % 2 !== 0; // clockwise or counterclockwise
