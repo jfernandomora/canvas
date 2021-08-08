837a80c52a9df425f4dd7704002bda7eeea7fe87
Fernando <jfernandomora@outlook.com>
2021-08-08 08:32:21 +0800

    Buttom canvas bigger

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function draw() {
       for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 3; j++) {
           ctx.beginPath();
-          var x = 25 + j * 50; // x coordinate
-          var y = 25 + i * 50; // y coordinate
-          var radius = 30; // Arc radius
+          var x = 25 + j * 100; // x coordinate
+          var y = 25 + i * 100; // y coordinate
+          var radius = 40; // Arc radius
           var startAngle = 0; // Starting point on circle
           var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
           var counterclockwise = i % 2 !== 0; // clockwise or counterclockwise
